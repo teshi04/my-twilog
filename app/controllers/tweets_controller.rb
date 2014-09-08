360d00a5809376dfd0eebeb4fa12976320edbeb0
teshi04 <teshi04@gmail.com>
2014-09-09 01:45:24 +0900

    Add Paging

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.page(params[:page]).per(100).order(:id)
   end
 
   # GET /tweets/1
